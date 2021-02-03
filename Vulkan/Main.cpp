129ee5edd0adbf326189cda2952ef110c50d2a8d
Jake Baum <jake.jakebaum.uk>
2021-02-03 01:42:08 +0000

    Displayes window, and creates Vulkan instance.

--- Vulkan/Main.cpp
+++ Vulkan/Main.cpp
@@ -1,4 +1,5 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
@@ -6,38 +7,88 @@
 
 class Application {
 public:
-  void run() {
-    initVulkan();
-    mainLoop();
-    cleanUp();
-  }
+	void run() {
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanUp();
+	}
 
 private:
-  void initVulkan() {
 
-  }
+	const uint32_t WIDTH = 800;
+	const uint32_t HEIGHT = 600;
 
-  void mainLoop() {
+	GLFWwindow* window;
+	VkInstance instance;
 
-  }
+	void initWindow() {
+		glfwInit();
 
-  void cleanUp() {
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-  }
- 
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Cool Title", nullptr, nullptr);
+	}
+
+	void initVulkan() {
+		createInstance();
+	}
+
+	void mainLoop() {
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+	}
+
+	void cleanUp() {
+		vkDestroyInstance(instance, nullptr);
+
+		glfwDestroyWindow(window);
+
+		glfwTerminate();
+	}
+
+	void createInstance() {
+		VkApplicationInfo appInfo {
+			VK_STRUCTURE_TYPE_APPLICATION_INFO,
+			nullptr,
+			"Cool Application",
+			VK_MAKE_VERSION(1, 0, 0),
+			"No Engine",
+			VK_MAKE_VERSION(1, 0, 0),
+			VK_API_VERSION_1_0
+		};
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("Failed to create instance");
+		}
+	}
+
 };
 
 
 int main() {
-  Application app;
+	Application app;
 
-  try {
-    app.run();
-  }
-  catch (const std::exception& e) {
-    std::cerr << e.what() << std::endl;
-    return EXIT_FAILURE;
-  }
+	try {
+		app.run();
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
